47a2b0afd7f78be97929f834b74d04e9d4f6cce9
Omarahm1 <omar2756389@gmail.com>
2024-04-14 20:09:07 +0100

    look up

--- headers/screens/GamePlay.hpp
+++ headers/screens/GamePlay.hpp
@@ -2,19 +2,20 @@
 #define GAMEPLAY_H
 #include <SFML/Graphics.hpp>
 
-int curmap  = 0;
 void gamePlayDraw(sf::RenderWindow &window);
 void gamePlayUpdate(sf::RenderWindow &window);
+/*
 struct blocks
 {
-    const float x_coordinate = 0, y_coordinate = 0,width = 0,height = 0;
+    float x_coordinate = 0, y_coordinate = 0,width = 0,height = 0;
+    sf::Texture block_shape;
 };
 struct mapss
 {
-    constexpr int num_of_blocks;
-    blocks block[9];
+    int num_of_blocks;
+    blocks block[6];
     sf::Texture background;
 };
-mapss map1[6];
-
+mapss mp[6];
+*/
 #endif // GAMEPLAY_H
