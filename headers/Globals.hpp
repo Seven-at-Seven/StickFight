d6456652ebb9484c02fb07acc16eb071a7d1a48d
Omarahm1 <omar2756389@gmail.com>
2024-04-16 10:11:33 +0100

    I have made a the map's backgrounds and made the maps global and deleted not some unused images

--- headers/Globals.hpp
+++ headers/Globals.hpp
@@ -2,6 +2,7 @@
 #define S_GLOBALS_H
 #include <SFML/Graphics.hpp>
 #include <unistd.h>
+#include"screens/GamePlay.hpp"
 
 const int SCREENWIDTH = 1240;
 const int SCREENHEIGHT = 480;
@@ -10,5 +11,6 @@ extern int current_screen;
 extern int number_of_players;
 extern sf::Event event;
 extern int curmap;
+extern maps map[6];
 
 #endif // S_GLOBALS_H
--- headers/screens/GamePlay.hpp
+++ headers/screens/GamePlay.hpp
@@ -2,18 +2,23 @@
 #define GAMEPLAY_H
 #include <SFML/Graphics.hpp>
 
-//int curmap  = 0;
-void gamePlayDraw(sf::RenderWindow &window);
-void gamePlayUpdate(sf::RenderWindow &window);
-struct blocks
+struct Blocks
 {
-    const float x_coordinate = 0, y_coordinate = 0,width = 0,height = 0;
+    float x_coordinate = 0, y_coordinate = 0,width = 0,height = 0;
+    sf::Sprite block_sprite;
+    sf::Texture block_shape;
 };
 struct maps
 {
-   int num_of_blocks;
-    blocks block[9];
+    int num_of_blocks;
+    Blocks block[6];
     sf::Texture background;
+    sf::Sprite background_sprite;
+    float xdir,ydir;
 };
+// functions
 
+void declaringmap();
+void gamePlayDraw(sf::RenderWindow &window);
+void gamePlayUpdate(sf::RenderWindow &window);
 #endif // GAMEPLAY_H
